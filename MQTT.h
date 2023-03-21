f16547db15953f79e73879c7a89b5f8caf07ca2b
tonyasaad <106562245+tonyasaad@users.noreply.github.com>
2023-03-21 20:22:27 +0200

    Add files via upload

---
+++ MQTT.h
@@ -0,0 +1,19 @@
+/*
+ * MQTT.h
+ *
+ *  Created on: Mar 12, 2023
+ *      Author: Pola
+ */
+
+#ifndef HAL_MQTT_MQTT_H_
+#define HAL_MQTT_MQTT_H_
+
+
+#include "../../HELPERS/std_types.h"
+
+void MQTT_Connect(uint8* clientID );
+void MQTT_Publish(uint8* topic, uint8* msg);  //, we can add the Qo level
+//void MQTT_PUPLISH(uint8* topic ,uint8* msg);
+void MQTT_Subscribe(uint8* topic);
+void test(uint16 data);
+#endif /* HAL_MQTT_MQTT_H_ */
